3f079deeef17070eef9a29938ff06d4f972ca02d
marcosf <marcosflaviosilveira@gmail.com>
2016-11-24 00:47:30 -0200

    Definindo o comportamento dos Services Os 3 services que se comunicarão com os WebServices são: -AdicionaProdutoService -AtualizaPedidoService -ListaPedidosService

--- app/src/main/java/br/com/teste/xofome/xofomeadmin/service/AdicionaProdutoService.java
+++ app/src/main/java/br/com/teste/xofome/xofomeadmin/service/AdicionaProdutoService.java
@@ -1,8 +1,25 @@
 package br.com.teste.xofome.xofomeadmin.service;
 
+import android.app.IntentService;
+import android.content.Intent;
+
 /**
  * Created by marcosf on 23/11/2016.
  */
 
-public class AdicionaProdutoService {
+public class AdicionaProdutoService extends IntentService{
+    private static final String TAG = "serviceAdd";
+
+    public AdicionaProdutoService() {
+        super("AdicionaProdutoService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // Receber um produto
+        //Transformá-lo em Json (sua img tbm)
+        // salvar local
+        //Criar conexão com webService
+        //passar o produto pro webService
+    }
 }
--- app/src/main/java/br/com/teste/xofome/xofomeadmin/service/ListaPedidosService.java
+++ app/src/main/java/br/com/teste/xofome/xofomeadmin/service/ListaPedidosService.java
@@ -1,11 +1,25 @@
 package br.com.teste.xofome.xofomeadmin.service;
 
+import android.app.IntentService;
+import android.content.Intent;
+
 /**
  * Created by marcosf on 19/11/2016.
  */
 
-public class ListaPedidosService {
+public class ListaPedidosService extends IntentService{
 
+    private static final String TAG = "serviceList";
+    public ListaPedidosService() {
+        super("ListaPedidosService");
+    }
 
-
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //Cria conexão com o WebService
+        // Recebe a lista de pedidos em Json
+        // Transforma a lista pra object e salva no banco
+        // envia um broadCast pra chamar onResume da Main activity
+        // Salva no banco local os novos pedidos
+    }
 }
